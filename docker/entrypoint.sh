b056688cad6d7e7d19c06694c151bdfc69dee99f
Hereman Nicolas <nicolas.hereman@gmail.com>
2025-07-13 17:42:27 +0200

    feat: Ability to manage multiple DNS records in docker or k8s

--- docker/entrypoint.sh
+++ docker/entrypoint.sh
@@ -1,7 +1,10 @@
 while true; do
-  if ! /home/cloudflare-dns-updater -c "${CDU_CONFIGURATION_FILE}"; then
-    echo "ERROR: cloudflare-dns-updater failed."
-  fi
+  for config_file in ${CDU_CONFIGURATION_FILES}; do
+    echo "INFO: Executing cloudflare-dns-updater with configuration file ${config_file}"
+    if ! /home/cloudflare-dns-updater -c "${config_file}"; then
+      echo "ERROR: cloudflare-dns-updater failed for configuration file: ${config_file}."
+    fi
+  done
   echo "---"
   sleep "${CDU_EXEC_INTERVAL}"
 done
